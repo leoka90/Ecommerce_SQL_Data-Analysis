44e804602475872e4a11dc2a9856819c77469cae
Chandan Kumar Sah <sahchandan032@outlook.com>
2025-06-30 18:35:54 +0530

    Delete query1.sql

--- query1.sql
+++
@@ -1,8 +0,0 @@
-USE ecommerce_db;
-
-SELECT c.Name, SUM(o.TotalAmount) AS TotalSpent
-FROM Customers c
-JOIN Orders o ON c.CustomerID = o.CustomerID
-GROUP BY c.CustomerID
-ORDER BY TotalSpent DESC
-LIMIT 5;
