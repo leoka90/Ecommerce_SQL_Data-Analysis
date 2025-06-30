56134bbc2c52dac7e1b1189fa69cb42754b720bf
Chandan Kumar Sah <sahchandan032@outlook.com>
2025-06-30 18:36:39 +0530

    Delete query3.sql

--- query3.sql
+++
@@ -1,8 +0,0 @@
-USE ecommerce_db;
-
-# Get products that are priced above the average product price
-SELECT ProductName, Price
-FROM Products
-WHERE Price > (
-    SELECT AVG(Price) FROM Products
-);
